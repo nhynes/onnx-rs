3ac7a5111236f6c3694fd16d86efd92caf2a29e3
Nick Hynes <nhynes@berkeley.edu>
2018-02-07 10:39:29 -0800

    Fix deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 extern crate protobuf;
 
-pub mod onnx;
+mod onnx;
+
+pub const ONNX_SRC_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/third_party/onnx/onnx");
+
+pub mod proto {
+    #[allow(unused)]
+    use onnx::*;
+}
